dfb96c98df9b03cf50e55efb70b7f70aae3fb03c
Alen Thomas <alen.thomas@ucdconnect.ie>
2019-12-23 13:54:33 +0000

    docker setup

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# my project build script
+cd core
+mvn clean compile install package
+cd ../broadcaster
+mvn clean compile install package
+docker build -t broadcaster:latest .
+cd ../soccer
+mvn clean compile install package
+docker build -t soccer:latest .
+cd ../nba
+mvn clean compile install package
+docker build -t nba:latest .
+cd ../client
+mvn clean compile install package
+cd ..
+#docker-compose up
